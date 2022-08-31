5208ad828e5e5ce0e8cea28b682b457126a0a864
Francis R Scallatino <tup31915@temple.edu>
2022-08-30 22:42:48 -0400

    prints out files/dirs of current dir without an argument

--- tuls.c
+++ tuls.c
@@ -1,8 +1,32 @@
+/*this program takes either 0 or 1 arguments
+ * 
+ */
 #include <stdio.h>
 #include <dirent.h>
-#include <sys/ytpe.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 int main (int argc, char *argv[])
 {
+    if (argc == 1)
+    {
+        struct dirent **namelist;
+           int n;
 
+           n = scandir(".", &namelist, NULL, alphasort);
+           if (n == -1) {
+               perror("scandir");
+               exit(EXIT_FAILURE);
+           }
+           
+           while (n--) {
+               printf("%s\n", namelist[n]->d_name);
+               free(namelist[n]);
+           }
+           free(namelist);
+
+           exit(EXIT_SUCCESS);
+    } else {
+
+    }
 }
