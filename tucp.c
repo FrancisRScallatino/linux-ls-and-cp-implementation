25921f5f3b1c7890893682e586fcebf717c90780
Francis R Scallatino <tup31915@temple.edu>
2022-09-06 22:21:47 -0400

    throw error if first entry is directory

--- tucp.c
+++ tucp.c
@@ -7,11 +7,18 @@
 
 int main (int argc, char **argv)
 {
-    if(argc == 1 || argc == 2){
+    if(argc == 1 || argc == 2)
+    {
         perror("Must supply arguments>1 (e.g. ./tucp [arg 1] [arg 2]...)");
-    }else if (argc == 3){
-        
-    }else{
-        
+    }else if (argc == 3)
+    {
+        DIR *dir = opendir(argv[1]);
+        if(dir){
+            perror("First argument must not be a directory.");
+            exit(EXIT_FAILURE);
+        }
+    }else
+    {
+
     }
 }
